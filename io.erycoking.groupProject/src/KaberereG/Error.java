b08711d99a82b9d5007a0e78ce966bd11b860396
Erycoking_mfalme <erycoking360@gmail.com>
2017-05-01 09:59:09 +0300

    moved kaberere's file inside the project

---
+++ io.erycoking.groupProject/src/KaberereG/Error.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package KaberereG;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+public class Error {
+    public static void display(){
+        
+    Stage window=new Stage();
+    window.initModality(Modality.APPLICATION_MODAL);
+    window.setTitle("Error");
+    window.setMinWidth(300);
+    
+    Label label=new Label();
+        label.setText("Not a valid username or password");
+        
+   StackPane layout=new StackPane(label);
+   Scene scene=new Scene(layout);
+   
+   window.setScene(scene);
+   window.showAndWait();
+    
+    
+    
+}
+}
