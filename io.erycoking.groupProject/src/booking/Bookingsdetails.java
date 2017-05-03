509afea75656513ee16b69d9e1bcecbc87e8c93f
Matthews <softmatthews>
2017-05-03 12:40:59 +0300

    Added the Booking details bean

---
+++ io.erycoking.groupProject/src/booking/Bookingsdetails.java
@@ -0,0 +1,120 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package booking;
+
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+/**
+ *
+ * @author Matthews
+ */
+public class Bookingsdetails {
+
+    /*Declare the properties  we are to use ..of the equipments */
+    private final StringProperty Equipment; //equipoment name
+    private final StringProperty Orderedby;
+    private final IntegerProperty quantityordered;
+    private final StringProperty Fromtime;
+    private final StringProperty Totime;
+    private final IntegerProperty orderuserId;
+    private final IntegerProperty BookingId;
+    private final IntegerProperty IdEq;//this keeps the equipment cost
+    //private final DoubleProperty Quantity;
+
+    public Bookingsdetails(String Equipment, String Orderedby, Integer quantityordered, String Fromtime, Integer IdEq,String Totime,Integer orderuserId,Integer BookingId) {
+        this.Equipment = new SimpleStringProperty(Equipment);
+        this.Orderedby = new SimpleStringProperty(Orderedby);
+        this.quantityordered = new SimpleIntegerProperty(quantityordered);
+        this.Fromtime = new SimpleStringProperty(Fromtime);
+        this.IdEq = new SimpleIntegerProperty(IdEq);
+        this.Totime=new SimpleStringProperty(Totime);
+        this.orderuserId=new SimpleIntegerProperty(orderuserId);
+        this.BookingId=new SimpleIntegerProperty(BookingId);
+    }
+    //*********************************************************************************************//
+    //this is the main constructor we are using in this system
+    //**********************************************************************************************//
+    public Bookingsdetails(Integer IdEq,Integer quantityordered, String Orderedby,String Equipment,String Fromtime,String Totime,Integer orderuserId,Integer BookingId) {
+        this.Equipment = new SimpleStringProperty(Equipment);
+        this.Orderedby = new SimpleStringProperty(Orderedby);
+        this.quantityordered = new SimpleIntegerProperty(quantityordered);
+        this.Fromtime = new SimpleStringProperty(Fromtime);
+        this.IdEq = new SimpleIntegerProperty(IdEq);
+        this.Totime=new SimpleStringProperty(Totime);
+        this.orderuserId=new SimpleIntegerProperty(orderuserId);
+        this.BookingId=new SimpleIntegerProperty(BookingId);
+    }
+    public void setEquipment(String value) {
+        Equipment.set(value);
+    }
+  public void setOrderedby(String value){
+    Orderedby.set(value);
+  }
+  public void setquantityordered(Integer value){
+      quantityordered.set(value);
+  }
+  public void setFromtime(String value){
+  Fromtime.set(value);
+  }
+  public void setTotime(String value){
+  Totime.set(value);
+  }
+  public void setIdEq(Integer value){
+  IdEq.set(value);
+  }
+   public void setuserorderId(Integer value){
+      orderuserId.set(value);
+  }
+    public void setBookingId(Integer value){
+      BookingId.set(value);
+  }
+
+    public String getEquipment() {
+        return Equipment.get();
+    }
+    /* public Integer getId() {
+        return Id.get();
+    }
+
+    public Integer getQuantity() {
+        return quantity.get();
+    }
+
+    public String getAvailable() {
+        return Available.get();
+    }
+*/
+
+    public String getOrderedby() {
+        return Orderedby.get();
+    }
+
+    public Integer getQuantityordered() {
+        return quantityordered.get();
+    }
+
+    public String getFromtime() {
+        return Fromtime.get();
+    }
+     public String getTotime() {
+        return Totime.get();
+    }
+
+    public Integer getIdEq() {
+        return IdEq.get();
+    }
+    public Integer getorderuserId() {
+        return orderuserId.get();
+    }
+    public Integer getBookingId() {
+        return BookingId.get();
+    }
+    
+
+}
