3c32c9f55c07489866b35a5983fe8f17c9917fa7
makwata <makwata@makwata-masters>
2017-05-11 08:07:45 +0300

    added changes on groups expect the GUI soon over the weekend people

---
+++ io.erycoking.groupProject/src/groups/UI.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package groups;
+
+/**
+ *
+ * @author makwata
+ */
+public class UI {
+    // TODO working on the views update soonest by next week thursday everything will be in ship shape.
+    
+}
