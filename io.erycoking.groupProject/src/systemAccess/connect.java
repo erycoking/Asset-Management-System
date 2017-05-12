8b448d5da5ad6ff8708adfdf63203c85a35ff197
KaberereG <gkaberere95@gmail.com>
2017-05-12 09:47:00 +0300

    Working add

--- io.erycoking.groupProject/src/systemAccess/connect.java
+++ io.erycoking.groupProject/src/systemAccess/connect.java
@@ -19,42 +19,42 @@ public class connect{
     private static Connection con;
     private static Statement st;
     private static ResultSet rs;
-     public connect() throws SQLException {
-    getConnection();
-     }
+     public connect(){
+    //getConnection();
+     
          
-  public static Connection getConnection() throws SQLException{
+ // public static Connection getConnection() throws SQLException{
     try{
          Class.forName("com.mysql.jdbc.Driver"); 
          String url="jdbc:mysql://localhost:3306/equipment"+"?verifyServerCertificate=false"+"&useSSL=false"+"&requireSSL=false";
          con= DriverManager.getConnection(url,"grace","grace");
-        //st=con.createStatement();
+        st=con.createStatement();
         }
         catch(Exception ex){
             System.out.println("Error here");
         }
-  return con;
+ //return con;
     }
 
-   public static ObservableList<Equipment> getData() throws SQLException{
-      final ObservableList<Equipment> data=FXCollections.observableArrayList();
+   public void getData(){
+      //final ObservableList<Equipment> data=FXCollections.observableArrayList();
         try{
             String query="select * from equipment_table";
             rs=st.executeQuery(query);
-           // System.out.println("Records from database");
+           System.out.println("Records from database");
             while(rs.next()){
                 Integer callid=rs.getInt("CallID");
                 String name=rs.getString("name");
                 Integer quantity=rs.getInt("quantity");
                 Integer cost=rs.getInt("cost");
                 
-                data.add(new Equipment(callid,name,quantity,cost));
-             
+               // data.add(new Equipment(callid,name,quantity,cost));
+              System.out.println("CallID: "+callid+"  "+"name: "+name+"  "+"quantity: "+quantity+ "  "+"cost: "+cost);
             }
         }
         catch(Exception e){
             System.out.println("Error");
         }
-        return data;    }
+           }
     
 }
